2aa03814ad8fa241b00d52eb9149b34a1d51db2f
shital <shitaldeshmukh2410@gmail.com>
2023-01-11 11:20:01 +0530

    Basic steps
    I have created framework structure go ahead and clone it start with your task
    Thanks.

---
+++ DalalStreet1/src/myclass/MyClass1.java
@@ -0,0 +1,14 @@
+package myclass;
+
+public class MyClass1 
+{
+	public static void main(String [] args)
+	{
+		System.out.println("Added dependancy");
+		System.out.println("Extent report");
+		System.out.println("log4j");
+		System.out.println(" Basic stuff");
+		
+	}
+
+}
